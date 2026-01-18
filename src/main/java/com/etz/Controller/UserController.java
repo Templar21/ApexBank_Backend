ac180c7f44e45b3871d0002cf12d2e3690047687
Kojo.Mante-Dankwa <kojonhyira17@gmail.com>
2026-01-18 16:00:15 +0000

    Update

--- src/main/java/com/etz/Controller/UserController.java
+++ src/main/java/com/etz/Controller/UserController.java
@@ -1,8 +1,10 @@
 package com.etz.Controller;
 
 
+import com.etz.DTO.Request.LoginRequest;
 import com.etz.DTO.Request.RegisterRequest;
-import com.etz.DTO.Response.RegisterResponse;
+import com.etz.DTO.Response.AuthResponse;
+import com.etz.Exception.AuthException;
 import com.etz.Service.UserService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
@@ -26,7 +28,7 @@ public class UserController {
         try {
             String token = userService.register(registerRequest);
             if (token != null) {
-                RegisterResponse response = new RegisterResponse("User registered successfully", token);
+                AuthResponse response = new AuthResponse("User registered successfully", token);
                 return Response.status(Response.Status.CREATED).entity(response).build();
             } else {
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Registration failed").build();
@@ -36,10 +38,27 @@ public class UserController {
         }
     }
 
+
     @POST
     @Path("login")
-    public Response loginUser(){
-        Authentication
+    public Response loginUser(LoginRequest loginRequest) {
+        try {
+
+            String token = userService.login(loginRequest);
+            if (token != null) {
+                AuthResponse response = new AuthResponse("Login successful", token);
+                return Response.ok(response).build();
+            }
+        } catch (AuthException e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(e.getMessage())
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred")
+                    .build();
+        }
+        return null;
     }
 
 
