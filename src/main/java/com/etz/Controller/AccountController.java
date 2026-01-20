c34030fd097251c85743738e6199ce404a2a2a45
Kojo.Mante-Dankwa <kojonhyira17@gmail.com>
2026-01-20 12:39:03 +0000

    Update

--- src/main/java/com/etz/Controller/AccountController.java
+++ src/main/java/com/etz/Controller/AccountController.java
@@ -4,6 +4,8 @@ import com.etz.DTO.Request.DepositRequest;
 import com.etz.Entity.Account;
 import com.etz.Service.AccountService;
 import com.etz.Utils.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
@@ -14,8 +16,8 @@ import java.util.List;
 @Produces("application/json")
 @Consumes("application/json")
 @Path("account")
-public class AccountController {
 
+public class AccountController {
 
     private final AccountService accountService;
 
@@ -38,7 +40,7 @@ public class AccountController {
             //3.Set Savings account specific values
             account.setUserId(userId);
             account.setAccountType(Account.AccountType.SAVINGS); // Set Enum here
-            account.setAccountNumber(account.generateSAccountNumber()); //// Generate Savings Account Number
+            account.setAccountNumber(account.generateSAccountNumber()); /// Generate Savings Account Number
 
             double initialbalance = account.getBalance();
             if (!(initialbalance <= 50)) {
@@ -56,16 +58,15 @@ public class AccountController {
         }
     }
 
-
     @POST
     @Path("create/current")
     public Response createCurrentAccount(@HeaderParam("Authorization") String authHeader, Account account) {
         try {
             //Extract token from header
-            String token=authHeader.substring(7);
+            String token = authHeader.substring(7);
 
             //Get userId from the token
-            int userId=JwtUtil.getUserIdFromToken(token);
+            int userId = JwtUtil.getUserIdFromToken(token);
 
             //Set Current account specific values
             account.setUserId(userId);
@@ -78,55 +79,147 @@ public class AccountController {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return Response.status(Response.Status.CREATED)
-                .entity("Account created successfully")
-                .build();
+        return Response.status(Response.Status.CREATED).entity("Account created successfully").build();
     }
-
 
     @POST
     @Path("deposit")
     public Response deposit(@HeaderParam("Authorization") String authHeader, DepositRequest depositRequest) {
+
+        //1.Perform JWT Checks
         try {
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            if (!authHeader.startsWith("Bearer ") || authHeader == null) {
                 return Response.status(Response.Status.UNAUTHORIZED).entity("Missing or invalid Authorization header").build();
             }
+        } catch (NullPointerException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Missing or invalid Authorization header").build();
+        }
 
-            //1.Extract token from header
-            String token = authHeader.substring(7);
-
-            //2.Get userId from the token (This validates the token)
-            JwtUtil.getUserIdFromToken(token);
-
-            accountService.deposit(depositRequest.getAccountNumber(), depositRequest.getPin(), depositRequest.getAmount());
-            return Response.ok("Deposit successful").build();
-
-        } catch (RuntimeException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
-
-        } catch (Exception e) {
+        //2.Extract token from header and perform validation
+        String token = authHeader.substring(7);
+        try {
+            JwtUtil.validateToken(token);
+        } catch (ExpiredJwtException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Token has expired. Please login again").build();
+        } catch (JwtException e) {
             return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid token").build();
         }
+
+        //3.Get UserID from token and get all Accounts associated with userID
+        int userID = JwtUtil.getUserIdFromToken(token);
+        List<Account> accounts = accountService.listAccountsByUserId(userID);
+        for(Account account :accounts){
+            if(account.getAccountNumber().equals(depositRequest.getAccountNumber())){
+
+                //Deposit into account
+                accountService.deposit(depositRequest.getAccountNumber(), depositRequest.getPin(), depositRequest.getAmount());
+                return Response.ok("Deposit successful").build();
+            }
+        }
+                return Response.status(Response.Status.NOT_FOUND).entity("Account does not exist or does not belong to you").build();
+
+        }
+
+    @POST
+    @Path("withdraw")
+    public Response withdraw(@HeaderParam("Authorization") String authHeader, DepositRequest depositRequest) {
+
+        //1.Perform JWT Checks
+        try {
+            if (!authHeader.startsWith("Bearer ") || authHeader == null) {
+                return Response.status(Response.Status.UNAUTHORIZED).entity("Missing or invalid Authorization header").build();
+            }
+        } catch (NullPointerException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Missing or invalid Authorization header").build();
+        }
+
+        //2.Extract token from header and perform validation
+        String token = authHeader.substring(7);
+        try {
+            JwtUtil.validateToken(token);
+        } catch (ExpiredJwtException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Token has expired. Please login again").build();
+        } catch (JwtException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid token").build();
+        }
+
+        //3.Get UserID from token and get all Accounts associated with userID
+        int userID = JwtUtil.getUserIdFromToken(token);
+        List<Account> accounts = accountService.listAccountsByUserId(userID);
+
+        for(Account account :accounts){
+            if(account.getAccountNumber().equals(depositRequest.getAccountNumber())){
+
+                //Withdraw from account
+                if(accountService.getBalance(depositRequest.getAccountNumber())>=depositRequest.getAmount()){
+                    accountService.withdraw(depositRequest.getAccountNumber(), depositRequest.getPin(), depositRequest.getAmount());
+                }
+                else{
+                    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Insufficient Funds").build();
+                }
+
+                //Display current balance
+                return Response.ok("Withdrawal successful.Current balance is "+ accountService.getBalance(depositRequest.getAccountNumber())).build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).entity("Account does not exist or does not belong to you").build();
+
     }
-
-
 
     @GET
     @Path("all")
     public List<Account> listallAccounts() {
-            return accountService.listallAccounts();
+        return accountService.listallAccounts();
     }
-
 
     @GET
     @Path("{id}")
     public Response getAccountById(@PathParam("id") int userId) {
 
-       List<Account> accounts = accountService.listAccountsByUserId(userId);
-        if (accounts == null || accounts.isEmpty() || userId<=0) {
+        List<Account> accounts = accountService.listAccountsByUserId(userId);
+        if (accounts == null || accounts.isEmpty() || userId <= 0) {
             return Response.status(Response.Status.NOT_FOUND).entity("No accounts associated with User").build();
         }
         return Response.status(Response.Status.FOUND).entity(accounts).build();
-}
+    }
+
+    @GET
+    @Path("balance/{accountNumber}")
+    public Response getBalance(@HeaderParam("Authorization") String authHeader, @PathParam("accountNumber") String accountNumber) {
+
+        //1.Perform JWT Checks
+        try {
+            if (!authHeader.startsWith("Bearer ") || authHeader == null) {
+                return Response.status(Response.Status.UNAUTHORIZED).entity("Missing or invalid Authorization header").build();
+            }
+        } catch (NullPointerException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Missing or invalid Authorization header").build();
+        }
+
+        //2.Extract token from header and perform validation
+        String token = authHeader.substring(7);
+        try {
+            JwtUtil.validateToken(token);
+        } catch (ExpiredJwtException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Token has expired. Please login again").build();
+        } catch (JwtException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid token").build();
+        }
+
+        //3.Get userID from JWT
+       int userID=JwtUtil.getUserIdFromToken(token);
+
+        //4.List all accounts from UserID
+        List<Account> accounts = accountService.listAccountsByUserId(userID);
+
+        //5.Check if the List contains the account(Path Param)
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                double balance = accountService.getBalance(accountNumber);
+                return Response.ok(balance).build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).entity("Account does not exist or does not belong to you").build();
+    }
 
 }
