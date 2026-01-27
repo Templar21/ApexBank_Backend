1fce6860f380c412eabca3f0fffa095905a3ad7d
Kojo Nhyira Mante-Dankwa <kojonhyira17@gmail.com>
2026-01-27 11:54:15 +0000

    Change the expiration time

--- src/main/java/com/etz/Utils/JwtUtil.java
+++ src/main/java/com/etz/Utils/JwtUtil.java
@@ -1,9 +1,9 @@
 package com.etz.Utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
@@ -13,7 +13,7 @@ public class JwtUtil {
 
     private static final String SECRET_STRING = "s9A3kL8QvJmF2xC4R7N0WzZyT1E5bH+q";
     private static final Key KEY = Keys.hmacShaKeyFor(SECRET_STRING.getBytes());
-    private static final long EXPIRATION_TIME = 864_000_000;//1 day
+    private static final long EXPIRATION_TIME = 600_000;//1 day
 
 
     public static String generateToken(String username, int userId) {
@@ -27,11 +27,24 @@ public class JwtUtil {
     }
 
     public static Claims validateToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(KEY)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(KEY)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+        } catch (ExpiredJwtException e) {
+            // Token is valid but expired
+            System.out.println("JWT expired");
+            throw e;
+
+        } catch (JwtException e) {
+            // Invalid token (signature, malformed, unsupported, etc.)
+            System.out.println("Invalid JWT");
+            throw e;
+        }
+
     }
 
 
